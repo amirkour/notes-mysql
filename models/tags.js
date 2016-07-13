de703e31b380ef7fa643b073af1b9b4094d3800c
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-07-13 12:13:13 -0700

    migration up and running to stop null tags.name - need to update specs and model

--- models/tags.js
+++ models/tags.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Tags = sequelize.define('Tags', {
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING(32),
+      allowNull: false
+    }
   }, {
     classMethods: {
       associate: function(models) {
