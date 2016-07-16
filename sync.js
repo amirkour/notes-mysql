19bb584abf304b3d0dd1054f7f91cab6a24fcefc
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-07-16 09:40:55 -0700

    adding sync.js - a quick way to run sequelize#sync and see how the db schema turns out when i rely on sequelize (instead of migrations)

--- sync.js
+++ sync.js
@@ -1,3 +1,13 @@
+/*
+ * this script uses sequelize#sync to generate database schema
+ * based on model definitions.  it's not gauranteed to be consistent
+ * with the migration files, but for now, i'm making an honest
+ * attempt to try and keep the two methods in sync.
+ *
+ * for now,the preferred method of getting the database schema
+ * correct is to use migrations, but this script provides a convenient
+ * and quick way to test sequelize#sync
+ */
 var db = require('./models/index.js'),
 	sequelize = db.sequelize;
 
